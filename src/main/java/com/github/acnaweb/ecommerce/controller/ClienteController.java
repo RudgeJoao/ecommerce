bdee8851ac45d506deee259924bf765956c18723
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-05 22:46:07 -0300

    Criando o mapper e service

--- src/main/java/com/github/acnaweb/ecommerce/controller/ClienteController.java
+++ src/main/java/com/github/acnaweb/ecommerce/controller/ClienteController.java
@@ -1,19 +1,41 @@
 package com.github.acnaweb.ecommerce.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.acnaweb.ecommerce.model.Cliente;
+import com.github.acnaweb.ecommerce.service.ClienteService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClienteController {
+	
+	private final ClienteService clienteService;
+	private final ModelMapper modelMapper;
 	
 	@GetMapping("/ping")
 	public List<ClienteDTO> getAll() {
-		List<ClienteDTO> result =null;
+		List<Cliente> clientes = clienteService.getAll();
+		
+		List<ClienteDTO> result = clientes
+				.stream()
+				.map(this::map)
+				.collect(Collectors.toList());
+		
 		return result;
 	}
+	
+	 private ClienteDTO map(Cliente cliente) {
+		 ClienteDTO dto = modelMapper.map(cliente, ClienteDTO.class);
+		 return dto;
+	 }
 
 }
